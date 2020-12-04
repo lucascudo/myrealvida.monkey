1c224d19d2f88dfc4090cb41e4a5aefcd14a82d6
Lucas Rocha <13350752+lucascudo@users.noreply.github.com>
2020-12-04 10:05:22 +0000

    Create realvida.user.js

---
+++ realvida.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Real Vida Tampermonkey
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Adds subtotals to the movement history.
+// @author       https://github.com/lucascudo
+// @match        https://myrealvida.pt/resgates/ResgatesFinanceiros
+// @match        https://myrealvida.pt/seguros/capitalizacao
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    let total = 0;
+    setInterval(() =>Array.from(document.querySelectorAll(".movimentos-item")).reverse().forEach((movimento, idx, array) => {
+        if (movimento.children.length > 2) {
+            return;
+        }
+        let value = parseFloat(movimento.children[1].children[0].innerText.replace('€', '').replace(',', '.').trim());
+        let op = movimento.children[1].children[1].classList[3];
+        if (op === 'positivo') {
+            total += value;
+        } else if (op === 'negativo') {
+            total -= value * 1.01;
+        }
+        const subtotal = document.createElement('div');
+        subtotal.innerHTML = '<b>' + total.toFixed(2).toString().replace('.', ',') + ' €</b>';
+        subtotal.className = 'col-xs-4 col-sm-3 text-right';
+        movimento.appendChild(subtotal);
+        const header = document.querySelector('.movimentos-header');
+        if (idx === array.length - 1 && header.children.length === 2) {
+            const title = document.createElement('div');
+            title.innerText = 'SUBTOTAL';
+            title.className = 'col-xs-4 col-sm-3 movimento-valor';
+            header.appendChild(title);
+        }
+    }), 3000);
+})();
